cee1d2c961ca371a2342a45a9e8a81ad6897ad78
Gustavo <gustavohsr11@gmail.com>
2024-06-28 09:20:21 -0300

    Creating CSS

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -12,18 +12,18 @@ const Login = () => {
         event.preventDefault();
 
         alert("Enviano os dados:" + username + " - " + password);
-    }
+    };
 
     return (
         <div className="container">
             <form onSubmit={handleSubmit}>
                 <h1>Acesse o sistema</h1>
-                <div>
-                    <input type="email" placeholder="E-mail" 
+                <div className="input-field">
+                    <input type="email" placeholder="E-mail" required
                     onChange={(e) => setUsername(e.target.value)} />
                     <FaUser className="icon" />
                 </div>
-                <div>
+                <div className="input-field">
                     <input type="password" placeholder="Senha"
                     onChange={(e) => setPassword(e.target.value)} />
                     <FaLock className="icon" />
